33e8c3e9dc72f15918200934b93871818d536185
Mohamed-Hmidouch <ffa54202@gmail.com>
2025-03-11 20:03:29 +0100

    commancer les perssmions

--- app/Services/ExpenseService.php
+++ app/Services/ExpenseService.php
@@ -1,2 +1,31 @@
 <?php
 namespace App\Services;
+
+class ExpenseService
+{
+    public function create($data)
+    {
+        return auth()->user()->expenses()->create($data);
+    }
+
+    public function update($expense, $data)
+    {
+        $expense->update($data);
+        return $expense;
+    }
+
+    public function delete($expense)
+    {
+        $expense->delete();
+    }
+
+    public function getExpenses()
+    {
+        return auth()->user()->expenses;
+    }
+
+    public function getExpense($expense)
+    {
+        return $expense;
+    }
+}
--- app/Services/TagService.php
+++ app/Services/TagService.php
@@ -1,2 +1,32 @@
 <?php
 namespace App\Services;
+
+class TagService
+{
+
+    public function create($data)
+    {
+        return auth()->user()->tags()->create($data);
+    }
+
+    public function update($tag, $data)
+    {
+        $tag->update($data);
+        return $tag;
+    }
+
+    public function delete($tag)
+    {
+        $tag->delete();
+    }
+
+    public function getTags()
+    {
+        return auth()->user()->tags;
+    }
+
+    public function getTag($tag)
+    {
+        return $tag;
+    }
+}
